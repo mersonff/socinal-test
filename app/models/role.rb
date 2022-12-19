2ed14bdd64e975823ab1b1f895decfed58c4a3ee
Emerson Freitas <mersonsi@gmail.com>
2022-12-19 19:19:38 -0300

    feat: cria relacionamentos

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,5 @@
 class Role < ApplicationRecord
   validates :name, uniqueness: true, presence: true
+
+  has_and_belongs_to_many :users, join_table: :users_roles
 end
--- app/models/users_role.rb
+++ app/models/users_role.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :roles, join_table: :users_roles
+  has_many :users_roles, dependent: :destroy
+  has_many :roles, through: :users_roles
   has_many :executions, dependent: :destroy
 
   validates :nickname, presence: true, uniqueness: true
-
-  accepts_nested_attributes_for :roles, allow_destroy: true
 end
